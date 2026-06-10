b5724ab041ef38e1108fbf375ed04018fee5c223
Ruhan Freitas <ruhanfreitas1@outlook.com>
2026-06-10 16:47:39 -0300

    first commit

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,9 @@
+import Image, { type ImageProps } from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>hello, world</div>
+  );
+}
