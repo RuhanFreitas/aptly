a42ad6081f44076e2b2faf06d516c92e120aae88
Ruhan Freitas <ruhanfreitas1@outlook.com>
2026-06-12 17:23:39 -0300

    chore: add .vscode directory

--- apps/gateway/src/auth/clerk-auth.guard.ts
+++ apps/gateway/src/auth/clerk-auth.guard.ts
@@ -1,21 +1,25 @@
-import clerkClient from "@clerk/clerk-sdk-node";
-import { CanActivate, ExecutionContext, Injectable, Logger } from "@nestjs/common";
+import clerkClient from '@clerk/clerk-sdk-node'
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  Logger,
+} from '@nestjs/common'
 
 @Injectable()
 export class ClerkAuthGuard implements CanActivate {
-    private readonly logger = new Logger()
+  private readonly logger = new Logger()
 
-    async canActivate(context: ExecutionContext) {
-        const request = context.switchToHttp().getRequest()
-        const token = request.cookies.__session
+  async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest()
+    const token = request.cookies.__session
 
-        try {
-            await clerkClient.verifyToken(token)
-            return true
-        } catch (error) {
-            this.logger.error(error)
-            return false
-        }
-
+    try {
+      await clerkClient.verifyToken(token)
+      return true
+    } catch (error) {
+      this.logger.error(error)
+      return false
     }
+  }
 }
